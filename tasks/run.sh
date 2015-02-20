2a06f85ceb0aacffe43afa8f40afd0d70dba482c
Gustavo Dutra <gustavotkg@gmail.com>
2015-02-20 18:38:55 -0200

    Preload every module and task before running the code
    It will ensure it works with everything loaded. No one needs to care about
    requiring the module before using it. Requiring the tasks also ensure
    everything will work if files are assembled into a single executable file.

--- tasks/run.sh
+++ tasks/run.sh
@@ -1,4 +1,4 @@
-btask.run.run () {
+function btask.run.run () {
   local working_dir="$(b.get bang.working_dir)"
   local file="$1"
   shift
@@ -9,8 +9,20 @@ btask.run.run () {
   fi
 
   if b.path.file? "$file"; then
+    local base_path="$(dirname "$file")"
+
+    # Preloading modules and tasks
+    b.path.dir? "$base_path/modules" && _load_bash_files_from "$base_path/modules"
+    b.path.dir? "$base_path/tasks" && _load_bash_files_from "$base_path/tasks"
+
     . "$file"
   else
     b.abort "Could not run '$file' because it is not a file."
   fi
+}
+
+function _load_bash_files_from () {
+  for file_path in $(find "$1" -name '*.sh'); do
+    source "$file_path"
+  done
 }
