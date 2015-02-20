7d11b6318a67aeb3a17b6c8962aa71f0039f5305
Gustavo Dutra <gustavotkg@gmail.com>
2015-02-20 11:40:44 -0200

    Run every single test in a subshell
    Tests are now independent and do not affect the environment.

--- tests/modules/unittest_test.sh
+++ tests/modules/unittest_test.sh
@@ -7,14 +7,9 @@ function b.test.run_successfuly_returns_true_when_test_case_passes () {
   function a_success_case () {
     b.unittest.assert_equal 'a' 'a'
   }
-  local test_passed=$( (
-    _BANG_ASSERTIONS_PASSED=2
 
-    b.unittest.run_successfuly? a_success_case &>/dev/null
-    [ $? ] && [ $_BANG_ASSERTIONS_PASSED -eq 3 ]
-    echo $?
-  ) )
-  b.unittest.assert_success $test_passed
+  b.unittest.run_successfuly? a_success_case &>/dev/null
+  b.unittest.assert_success $?
 }
 
 function b.test.run_successfuly_returns_false_when_test_case_fails () {
@@ -22,12 +17,6 @@ function b.test.run_successfuly_returns_false_when_test_case_fails () {
     b.unittest.assert_equal 'a' 'b'
   }
 
-  local test_failed=$( (
-    _BANG_ASSERTIONS_FAILED=1
-
-    b.unittest.run_successfuly? a_fail_case &>/dev/null
-    [ $? ] && [ $_BANG_ASSERTIONS_FAILED -eq 2 ]
-    echo $?
-  ) )
-  b.unittest.assert_success $test_failed
+  b.unittest.run_successfuly? a_fail_case &>/dev/null
+  b.unittest.assert_error $?
 }
